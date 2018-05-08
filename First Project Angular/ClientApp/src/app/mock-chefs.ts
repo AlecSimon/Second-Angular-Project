4c72043f39578b772b45802a0ccc4bc2147066d6
TeridaxSSB <duncanmott66@gmail.com>
2018-05-08 15:03:36 -0400

    Bug fixes

--- First Project Angular/ClientApp/src/app/mock-chefs.ts
+++ First Project Angular/ClientApp/src/app/mock-chefs.ts
@@ -2,5 +2,6 @@ import { Chef } from './chef';
 
 export const CHEFS: Chef[] = [
     { firstName: "Jim", lastName: "Bob", day: 8, month: 5, year: 2018},
-    { firstName: "Wind", lastName: "Storm", day: 15, month: 5, year: 2018}
+    { firstName: "Wind", lastName: "Storm", day: 15, month: 5, year: 2018},
+    { firstName: "Bad", lastName: "Order", day: 12, month: 3, year: 2018}
 ]
