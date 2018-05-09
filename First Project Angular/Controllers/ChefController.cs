05047e344ea1e5177e1f4e71438933ff93f7707d
AlecSimon <39039076+AlecSimon@users.noreply.github.com>
2018-05-09 14:31:46 -0400

    Added controller

---
+++ First Project Angular/Controllers/ChefController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First_Project_Angular.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Chef")]
+    public class ChefController : Controller
+    {
+        [HttpGet]
+        public IActionResult Test()
+        {
+            return Ok("Hello");
+        }
+    }
+}
