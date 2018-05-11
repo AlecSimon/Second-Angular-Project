71b5515a5794c65a86d8698bbf2a3f9adb036a90
TeridaxSSB <duncanmott66@gmail.com>
2018-05-11 09:34:18 -0400

    controller test

--- First Project Angular/Controllers/ChefController.cs
+++ First Project Angular/Controllers/ChefController.cs
@@ -8,11 +8,30 @@ using Microsoft.AspNetCore.Mvc;
 namespace First_Project_Angular.Controllers
 {
     [Produces("application/json")]
-    [Route("api/Chef")]
+    [Route("api/[controller]")]
     public class ChefController : Controller
     {
-        [HttpGet]
-        public IActionResult Test() => Ok("Hello");
-        
+        [HttpGet("[action]")]
+        public IEnumerable<Chef> Chefs() {
+            var uwu = 0;
+            return Enumerable.Range(1, 5).Select(index => new Chef {
+                id = uwu + 1,
+                firstName = "Wind",
+                lastName = "Storm",
+                day = 24,
+                month = 3,
+                year = 2000
+            });
+        }
+
+        public class Chef
+        {
+            public int id { get; set; }
+            public string firstName { get; set; }
+            public string lastName { get; set; }
+            public int day { get; set; }
+            public int month { get; set; }
+            public int year { get; set; }
+        }
     }
 }
